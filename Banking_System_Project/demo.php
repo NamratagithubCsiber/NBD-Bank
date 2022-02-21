5ec98e16e40eac25c442286eb9ed0fb18142fd25
NamratagithubCsiber <98451828+NamratagithubCsiber@users.noreply.github.com>
2022-02-21 17:30:22 +0530

    Add files via upload

---
+++ Banking_System_Project/demo.php
@@ -0,0 +1,19 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database="customer_info";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password,$database);
+
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+else
+{
+echo "Connected successfully";
+}
+
+?>
